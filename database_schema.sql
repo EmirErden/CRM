2f642ccb01bd394326214793e5368a51c0656292
Emir Erden <103561700+EmirErden@users.noreply.github.com>
2025-01-23 17:02:36 +0300

    Changed the database

--- database_schema.sql
+++ database_schema.sql
@@ -1,54 +1,55 @@
-
-CREATE TABLE CustomerPeople -- Müşterilerin bilgilerinin bulunduğu tablo
+CREATE TABLE Accounts -- Müşteriler ve şirketlerin bilgilerini içeren tablo
 (
-    customer_id SERIAL PRIMARY KEY, --Müşteriye özel Id'si
-    name VARCHAR(50) NOT NULL, --İsmi
-    email VARCHAR(50) NOT NULL, --E-mail'i
-    phone VARCHAR(15) NOT NULL, --Telefon Numarası
-    address VARCHAR(255) NOT NULL, --Adresi
-    city VARCHAR(50) NOT NULL, --Şehri
-    country VARCHAR(50) NOT NULL, --Ülkesi
-    date_of_birth DATE NOT NULL, --Doğum tarihi
-    gender VARCHAR(20) NOT NULL, --Cinsiyeti
-    martial_status BOOLEAN DEFAULT FALSE,
-    have_kids BOOLEAN DEFAULT FALSE,
-    registration_date DATE DEFAULT CURRENT_DATE --Kayıt olma tarihi
+    account_id SERIAL PRIMARY KEY, -- Hesaba özel Id
+    is_company BOOLEAN,
+    account_type VARCHAR(20) NOT NULL, -- Müşteri Türü
+    company_type VARCHAR(30),
+    contact_name VARCHAR(30),
+    name VARCHAR(100) NOT NULL, -- İsim veya Şirket Adı
+    email VARCHAR(50) NOT NULL, -- E-mail
+    phone VARCHAR(15) NOT NULL, -- Telefon Numarası
+    address VARCHAR(255) NOT NULL, -- Adres
+    city VARCHAR(50) NOT NULL, -- Şehir
+    country VARCHAR(50) NOT NULL, -- Ülke
+    date_of_birth DATE, -- Doğum tarihi (kişiler için geçerli)
+    gender VARCHAR(20), -- Cinsiyet (kişiler için geçerli)
+    martial_status BOOLEAN DEFAULT FALSE, -- Medeni durum (kişiler için geçerli)
+    have_kids BOOLEAN DEFAULT FALSE, -- Çocuk sahibi olma durumu (kişiler için geçerli)
+    industry VARCHAR(50), -- Şirketin endüstrisi (şirketler için geçerli)
+    website VARCHAR(50), -- Web sitesi (şirketler için geçerli)
+    linkedin_url VARCHAR(50), -- LinkedIn URL (şirketler için geçerli)
+    instagram_url VARCHAR(50), -- Instagram URL (şirketler için geçerli)
+    facebook_url VARCHAR(50), -- Facebook URL (şirketler için geçerli)
+    twitter_url VARCHAR(50),
+    no_of_employees INT, -- Çalışan sayısı (şirketler için geçerli)
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, --Potansiyel müşteri olduğu tarih
+    created_by VARCHAR(50) NOT NULL,
+    last_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_by VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE CustomerCompanies --Şirket ile anlaşan diğer şirketlerin bilgilerinin olduğu tablo
-(
-    company_id SERIAL PRIMARY KEY,      --Company'e özel ID
-    company_name VARCHAR(100) NOT NULL, --İsmi
-    industry VARCHAR(50) NOT NULL,      --Şirketin Endüstrisi
-    website VARCHAR(50) NOT NULL,       --Websitesi
-    email VARCHAR(50) NOT NULL ,        --E-mail adresi
-    phone VARCHAR(15) NOT NULL,         --Telefon Numarası
-    address VARCHAR(255) NOT NULL,      --Adresi
-    city VARCHAR(50) NOT NULL,          --Bulunduğu il
-    country VARCHAR(50) NOT NULL,       --Bulunduğu ülke
-    linkedin_url VARCHAR(50),
-    instagram_url VARCHAR(50),
-    facebook_url VARCHAR(50),
-    no_of_employees INT,
-    registration_date DATE DEFAULT CURRENT_DATE
-);
 
 CREATE TABLE Interactions --Müşteriler ile olan etkileşimlerin bulunduğu masa
 (
     interaction_id BIGSERIAL PRIMARY KEY, --Etkileşime özel Id
-    customer_id INT, --Müşteri ile etkileşime geçildiyse müsterinin Id'si yazılır. Burada Foreign Key kısıtlaması vardır.
-    company_id INT, --Şirket ile etkileşime geçildiyse şirketin Id'si yazılır. Burada Foreign Key kısıtlaması vardır.
+    account_id INT, --Müşterilerin Id'si. Burası Foreign Key olacak.
     interaction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP, --Etkileşimin yapıldığı zaman
     interaction_type VARCHAR(50), --Etkileşimin çeşidi(servis, bug vb.)
     interaction_channel VARCHAR(50), --Etkileşimin hangi metodla yapıldığı yazılır
     notes TEXT NOT NULL, --Etkileşimden alınabilecek notlar.
-    FOREIGN KEY (customer_id) REFERENCES CustomerPeople(customer_id),
-    FOREIGN KEY (company_id) REFERENCES CustomerCompanies (company_id)
+    FOREIGN KEY (account_id) REFERENCES Accounts(account_id)
 );
 
 CREATE TABLE Opportunities
 (
-
+    opportunity_id SERIAL PRIMARY KEY,
+    opportunity_name VARCHAR(50) NOT NULL,
+    account_name VARCHAR(50) NOT NULL,
+    owner_full_name VARCHAR(50) NOT NULL,
+    amount INT,
+    close_date DATE,
+    status VARCHAR(20) DEFAULT 'Qualification',
+    probability INT CHECK ( probability <= 100 )
 );
 
 CREATE TABLE Leads --Potansiyel müşterilerin bilgilerinin bulunduğu tablo
@@ -87,14 +88,12 @@ CREATE TABLE Products --Ürünlerin bulunduğu tablo
 CREATE TABLE Orders --Siparişlerin bulunduğu tablo
 (
     order_id SERIAL PRIMARY KEY, --Siparişe özel Id
-    customer_id INT NOT NULL, --Sipariş yapan müşterinin Id'si. Burası customer tablosunun customer_id alanına foreign key olacak
+    account_id INT NOT NULL, --Sipariş yapan müşterinin Id'si. Burası customer tablosunun customer_id alanına foreign key olacak
     order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP, --Sipariş tarihi
     total_amount DECIMAL(10,2) NOT NULL, --Sipariş miktarı
-    status VARCHAR(50) NOT NULL --Sipariş durumu
+    status VARCHAR(50) NOT NULL, --Sipariş durumu
+    FOREIGN KEY (account_id) REFERENCES Accounts(account_id)
 );
-
---Orders tablosuna customer_id Foreign Key'inin ekleme sorgusu
-ALTER TABLE Orders ADD CONSTRAINT fk_customer_id FOREIGN KEY (customer_id) REFERENCES CustomerPeople(customer_id);
 
 CREATE TABLE OrderDetails --Siparişlerin daha detaylı bilgilernin olduğu tablo
 (
@@ -124,38 +123,27 @@ CREATE TABLE Users --Kullanıcıların bulunduğu tablo
 CREATE TABLE SupportTickets --Kullanıcıların destek biletlerinin tutulduğu yer
 (
     ticket_id SERIAL PRIMARY KEY, --Biletlere özel Id
-    customer_id INT, --Bileti customer yaptıysa customer_id'si olacak
-    company_id INT, --Bileti şirket yaptıysa company_id olacak
+    account_id INT, --Bileti yapan Müşterinin Id'si. Burası foreign key olacak.
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, --Biletin yapılma zamanı
     priority VARCHAR(20) NOT NULL, --Biletin öncelik düzeyi
     status VARCHAR(20) NOT NULL DEFAULT 'Open', --Biletin durumu
     issue_description TEXT NOT NULL, --Bilette anlatılan sorun
     solution_description TEXT, --Eğer çözüldüyse sorunun çözümü
-    solution_date TIMESTAMP --Eğer çözüldüyse çözülme zamanı
+    solution_date TIMESTAMP, --Eğer çözüldüyse çözülme zamanı
+    FOREIGN KEY (account_id) REFERENCES Accounts(account_id)
 );
 
---SupportTicket tablosuna customer_id foreign Key'ini ekleme sorgusu
-ALTER TABLE SupportTickets ADD CONSTRAINT fk_customer_id FOREIGN KEY (customer_id) REFERENCES CustomerPeople(customer_id);
-
---SupportTicket tablosuna company_id Foreign Key'ini ekleme sorgusu
-ALTER TABLE SupportTickets ADD CONSTRAINT fk_company_id FOREIGN KEY (company_id) REFERENCES CustomerCompanies (company_id);
 
 CREATE TABLE Feedbacks --Müşterilerin(şirketlerin ve insanların) geri dönüşlerinin tutulduğu tablo
 (
     feedback_id SERIAL PRIMARY KEY, --Feedback'e özel Id
-    customer_id INT, --Geri dönüşü müşteri yaptıysa customer_id doldurulacak (burası foreign key olacak)
-    company_id INT, --Geri dönüşü şirket yaptıysa company_id doldurulacak (burası foreign key olacak)
+    account_id INT, --Bileti yapan Müşterinin Id'si. Burası foreign key olacak.
     feedback_type VARCHAR(50) NOT NULL, --Şirketimizin hangi kısmına geri dönüş yapıyor (Servis, hizmet vb.)
     rating INT NOT NULL, --1 ile 5 arasında puan
     comment TEXT, --Yapmal isterse yorum sekmesi
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP --Geri dönüşün yapıldığı zaman
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, --Geri dönüşün yapıldığı zaman
+    FOREIGN KEY (account_id) REFERENCES Accounts(account_id)
 );
-
---Feedbacks tablosuna customer_id foreign key'ini ekleme sorgusu
-ALTER TABLE Feedbacks ADD CONSTRAINT fk_customer_id FOREIGN KEY (customer_id) REFERENCES CustomerPeople(customer_id);
-
---Feedbacks tablosuna company_id Foreign Key'ini ekleme sorgusu
-ALTER TABLE Feedbacks ADD CONSTRAINT fk_company_id FOREIGN KEY (company_id) REFERENCES CustomerCompanies (company_id);
 
 
 
